96b03872e864a141f7f7c21b8ba0939709d70659
Amit Chahar <amit.chahar@wispero.com>
2018-05-27 10:23:49 +0530

    bluetooth basic code added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pi-ble-shell
+
+**This project is currently under development.**
+
+**Feel free to contribute.**
+
 This repository contains the BLE shell (BSH) code for the Raspberry Pi.
+
+**pi-ble-shell** is a part of the projet **pibluetooth**. The objective of the **pibluetooth** project is to enable access (just like ssh) to Raspberry Pi shell from the PC using bluetooth interface.
+
+## Installation Instructions
+1. Git clone this directoy to your Raspberry Pi.
+2. Change to the pi-ble-shell directory.
+3. Run make command.
---
+++ main.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2018 Amit Chahar
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+from bluetooth import *
+
+server_sock = BluetoothSocket(RFCOMM)
+server_sock.bind(("", PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "347fb489-38fb-4325-898b-ec28b40c3c46"
+
+advertise_service(server_sock, "Raspberry-Pi",
+                  service_id=uuid,
+                  service_classes=[uuid, SERIAL_PORT_CLASS],
+                  profiles=[SERIAL_PORT_PROFILE],
+                  )
+
+print("Raspberry Pi BLE shell started")
+print("Waiting for connections...")
+print("Waiting for connection on RFCOMM channel %d" % port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from ", client_info)
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if len(data) == 0:
+            break
+        print("received [%s]" % data)
+except IOError:
+    pass
+
+print("disconnected")
+
+client_sock.close()
+server_sock.close()
+print("all done")
